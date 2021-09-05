611fc748574d1177d969e9fa59d82cd75334d7ef
tehma <tehma@LEGION>
2021-09-05 18:44:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Print a number for each line" + i);
+            }
+
+        }
+    }
+}
